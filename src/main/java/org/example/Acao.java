2bd1eb242a2e1ffa47b90e0ebe58f0780df0f4eb
adrianm07 <adrianmaciel007@gmail.com>
2025-05-30 20:15:01 -0300

    Cria o movimentaçao

--- src/main/java/org/example/Acao.java
+++ src/main/java/org/example/Acao.java
@@ -1,5 +1,12 @@
 package org.example;
 
-public class Acao {
+import java.util.Scanner;
 
+public class Acao {
+    public void movimentacao(Tabuleiro tabuleiroAtual, Personagem personagemAtual) {
+        Scanner teclado = new Scanner(System.in);
+        String acao = teclado.nextLine();
+        tabuleiroAtual.setPos(personagemAtual, acao , personagemAtual.getIndicaPlayer());
+        tabuleiroAtual.imprimeTabuleiro();
+    }
 }

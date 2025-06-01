f4a68f4b12764301d6a35b8daf70496f7649b1ba
adrianm07 <adrianmaciel007@gmail.com>
2025-06-01 02:47:45 -0300

    cria opçao de sair no meio do jogo

--- src/main/java/org/example/Jogo.java
+++ src/main/java/org/example/Jogo.java
@@ -4,17 +4,21 @@ import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Jogo {
-
+    private Boolean querSair;//cria variavel pra sair do jogo
+    public Jogo() {
+        querSair = true;
+    }
     public void jogo(Personagem p1, Personagem p2) {
         Personagem atual = p1;
         Tabuleiro tabuleiro1 = new Tabuleiro(p1,p2);
+
         tabuleiro1.imprimeTabuleiro();
         //rodada(p1,tabuleiro1);
         //rodada(p2,tabuleiro1);
         //tabuleiro1.setPos(p1, "a", p1.getIndicaPlayer());
         //tabuleiro1.imprimeTabuleiro();
 
-        while (p1.getPontoVida() > 0 && p2.getPontoVida()>0){
+        while (p1.getPontoVida() > 0 && p2.getPontoVida() > 0 && querSair){
             System.out.println("Vida de " + p1.getNome() + ": " + p1.getPontoVida());
             System.out.println("Vida de " + p2.getNome() + ": " + p2.getPontoVida());
             System.out.println(" ");
@@ -35,7 +39,7 @@ public class Jogo {
     public void rodada(Personagem p, Tabuleiro tabuleiro1){
         Acao acao = new Acao();
         System.out.println("ESCOLHA SUA AÇÃO:");
-        System.out.println("1 - MOVIMENTAR | 2 - ATACAR | 3 - DEFENDER | 4 - ATAQUE ESPECIAL");
+        System.out.println("1 - MOVIMENTAR | 2 - ATACAR | 3 - DEFENDER | 4 - ATAQUE ESPECIAL | 5 - SAIR");
         Scanner teclado = new Scanner(System.in);
         String escolha = teclado.nextLine();
         boolean teste = false;
@@ -52,6 +56,9 @@ public class Jogo {
                     break;
                 case "4":
                     acao.ultimate(p);
+                    break;
+                case "5":
+                    querSair = false;//se escolher "5" da false e quebra o while
                     break;
                 default:
                     teste = true;
